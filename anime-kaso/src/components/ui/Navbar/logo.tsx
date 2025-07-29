176fabfb13b541e105169400ab7dd8ac47468687
kawji <24.supavich@gmail.com>
2025-07-29 22:43:42 +0700

    set logo component

---
+++ anime-kaso/src/components/ui/Navbar/logo.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Logo() {
+    return(
+        <>
+            <span className="text-3xl font-bold ">
+                Anime&nbsp;-&nbsp;
+            </span>
+            <span className="text-[var(--primary-text)] text-3xl font-bold ">
+                Kaso.com
+            </span>
+        </>
+    );
+}
