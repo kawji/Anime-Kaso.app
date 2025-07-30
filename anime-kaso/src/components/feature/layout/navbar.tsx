b56413b704f65199a25ab2df831d79c53b230c0c
kawji <24.supavich@gmail.com>
2025-07-30 23:25:51 +0700

    set navbar in feature

---
+++ anime-kaso/src/components/feature/layout/navbar.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/components/ui/Navbar/logo";
+import SearchAnime from "@/components/ui/Navbar/search";
+
+export default function Navbar() {
+    return(
+        <nav className="fixed flex items-center px-10 bg-[var(--primary-bg)] w-full sm:h-16 top-0 left-0 border-b border-b-white/35 "  >
+            <Logo />
+            <SearchAnime />
+        </nav>
+    );
+}
