a1b293f1249489374f025dc4ab7f0fcf2f5522cb
kawji <24.supavich@gmail.com>
2025-07-30 23:27:18 +0700

    set config of sidebar

---
+++ anime-kaso/src/config/sidebarMenu.ts
@@ -0,0 +1,10 @@
+export const sidebarMenu = [
+    {section:'หน้าแรก', path:'/'},
+    {section:'อนิเมะทั้งหมด', path:'/anime-all'},
+    {section:'ซับไทย', path:'/sub-thai'},
+    {section:'พากย์ไทย', path:'/thai'},
+    {section:'อนิเมะ - Hentai', path:'/anime-hentai'},
+    {section:'อนิเมะ - Movie', path:'/anime-movie'},
+    {section:'ยังไม่จบ', path:'/no-end'},
+    {section:'จบแล้ว', path:'/end'},
+]
