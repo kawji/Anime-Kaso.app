92afcea2996eb49b37baed3a28f21c0a5a597cb8
kawji <24.supavich@gmail.com>
2025-07-31 22:54:13 +0700

    add z-index navbar

--- anime-kaso/src/components/feature/layout/navbar.tsx
+++ anime-kaso/src/components/feature/layout/navbar.tsx
@@ -1,9 +1,9 @@
-import Logo from "@/components/ui/Navbar/logo";
-import SearchAnime from "@/components/ui/Navbar/search";
+import Logo from "@/components/ui/navbar/logo";
+import SearchAnime from "@/components/ui/navbar/search";
 
 export default function Navbar() {
     return(
-        <nav className="fixed flex items-center px-10 bg-[var(--primary-bg)] w-full sm:h-16 top-0 left-0 border-b border-b-white/35 "  >
+        <nav className="fixed z-100 flex items-center px-10 bg-[var(--primary-bg)] w-full sm:h-16 top-0 left-0 border-b border-b-white/35 "  >
             <Logo />
             <SearchAnime />
         </nav>
