c99f5bea14bc7ddd5f30e4517c77274e23a4ce1f
kawji <24.supavich@gmail.com>
2025-07-31 22:54:39 +0700

    edit padding

--- anime-kaso/src/components/feature/layout/sidebar.tsx
+++ anime-kaso/src/components/feature/layout/sidebar.tsx
@@ -2,13 +2,13 @@
 import { usePathname } from "next/navigation";
 import { sidebarMenu } from "@/config/sidebarMenu";
 // ui
-import ButtonSidebar from "@/components/ui/Sidebar/button";
-import TitleSidebar from "@/components/ui/Sidebar/title";
+import ButtonSidebar from "@/components/ui/sidebar/button";
+import TitleSidebar from "@/components/ui/sidebar/title";
 
 export default function Sidebar() {
     let namePath = usePathname();
     return(
-        <aside  className="flex flex-col gap-2 left-0 w-full lg:w-[20vw] py-25 h-screen bg-black  ">
+        <aside  className="flex flex-col gap-2 left-0 w-full lg:w-[20vw] pt-[var(--start-top)] pl-[var(--start-left)] h-screen bg-black  ">
             <TitleSidebar title='หมวดหมู่' />
 
             {sidebarMenu.map((item) =>{
