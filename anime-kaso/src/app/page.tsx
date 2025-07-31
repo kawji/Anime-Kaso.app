48e33e8162f4dcae56256bbe4f12431dcdc75210
kawji <24.supavich@gmail.com>
2025-07-31 22:53:47 +0700

    set page home in path /

--- anime-kaso/src/app/page.tsx
+++ anime-kaso/src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/feature/layout/navbar";
 import Sidebar from "@/components/feature/layout/sidebar";
+import MainContent from "@/components/feature/layout/mainContent";
 
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen pb-20  flex">
-      <Navbar />
+    <div className="font-sans min-h-screen pb-20 flex">
       <Sidebar />
+      <MainContent />
     </div>
   );
 }
