38befc95945345fd926f40a5a6ec00fd605f8308
kawji <24.supavich@gmail.com>
2025-08-01 23:54:54 +0700

    create button carousel

---
+++ anime-kaso/src/components/ui/main-content/carousel/buttonCarousel.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ButtonCarousel() {
+    return(
+        <div className="p-8 flex justify-center items-end shrink-0 ">
+            <button className="px-9 py-[7px] bg-[var(--color-primary)] rounded-md text-black text-[16px] transition-all group-hover:bg-[#B0CAE2] group-hover:text-[var(--color-dense)] ">รับชมตอนนี้</button>
+        </div>
+    );
+}
