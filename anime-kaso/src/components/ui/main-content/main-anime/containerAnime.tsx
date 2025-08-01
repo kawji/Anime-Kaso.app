327b2deff0f73fc9e6c2de141d61fd330bd7acd6
kawji <24.supavich@gmail.com>
2025-08-01 23:57:29 +0700

    add container anime becors remove

---
+++ anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
@@ -0,0 +1,12 @@
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function ContainerAnime({ children }:Props ) {
+    return(
+        <article className="max-w-[1200px]  h-auto grid grid-cols-[repeat(auto-fit,minmax(160px,220px))] lg:gap-3 gap-2 " >
+            {children}
+        </article>
+    );
+}
