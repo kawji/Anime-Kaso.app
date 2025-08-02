8ce6ccf987681dae5a5faaa949dd91154e3e7822
kawji <24.supavich@gmail.com>
2025-08-02 11:50:14 +0700

    addd data to config

---
+++ anime-kaso/src/config/guideAnimeMenu.ts
@@ -0,0 +1,7 @@
+
+export const guideAnimeMenu = [
+    {title:'Demon Slayer- Kimetsu no Yaiba Season 1' ,subtitle:'ซับไทย' ,chapter:24 ,path:'tanjiro1.jpg'},
+    {title:'Solo Leveling Season 1' ,subtitle:'ซับไทย' ,chapter:12 ,path:'solo.jpg'},
+    {title:'My Hero Academia FINAL SEASON' ,subtitle:'ซับไทย' ,chapter:1 ,path:'midoriya.jpg'}
+
+]
