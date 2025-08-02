ec414b12e4783f3b6a5739711b5532cadf2edb79
kawji <24.supavich@gmail.com>
2025-08-02 11:47:40 +0700

    set caption add prop detail component

--- anime-kaso/src/components/ui/main-content/carousel/captionCarousel.tsx
+++ anime-kaso/src/components/ui/main-content/carousel/captionCarousel.tsx
@@ -1,10 +1,13 @@
+'use client'
 import DetailCarousel from "./detailCarousel";
 import ButtonCarousel from "./buttonCarousel";
+import { guideAnimeDetailType } from "@/type/guideAnime";
 
-export default function CaptionCarousel() {
+
+export default function CaptionCarousel({ title ,subtitles ,chapter }:guideAnimeDetailType) {
     return(
         <figcaption className="flex absolute right-0 bottom-0 w-[75%] h-[28%] bg-black/50 backdrop-blur-[4px] rounded-tl-2xl shadow-lg shadow-blue-500 " >
-            <DetailCarousel title='Demon Slayer- Kimetsu no Yaiba Season 1' subtitles="ซับไทย" chapter={24} />
+            <DetailCarousel title={title} subtitles={subtitles} chapter={chapter} />
             <ButtonCarousel />
         </figcaption>
     );
