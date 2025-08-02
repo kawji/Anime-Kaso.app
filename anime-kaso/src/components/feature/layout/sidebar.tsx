1335bcaa7336635aaf4e04b2fc43e6311c5dc968
kawji <24.supavich@gmail.com>
2025-08-02 22:52:44 +0700

    add top anime

--- anime-kaso/src/components/feature/layout/sidebar.tsx
+++ anime-kaso/src/components/feature/layout/sidebar.tsx
@@ -1,19 +1,28 @@
 'use client'
 import { usePathname } from "next/navigation";
 import { sidebarMenu } from "@/config/sidebarMenu";
+import { topAnimeList } from "@/config/topAnime";
+import { v4 as uuidv4 } from 'uuid';
 // ui
 import ButtonSidebar from "@/components/ui/sidebar/button";
 import TitleSidebar from "@/components/ui/sidebar/title";
+import CardTopAnime from "@/components/ui/sidebar/cardTopAnime";
+
 
 export default function Sidebar() {
     let namePath = usePathname();
     return(
-        <aside  className="flex flex-col gap-2 left-0 shrink-0 sm:w-[260px] pt-[var(--start-top)] pl-[var(--start-left)] h-screen bg-black relative z-99 ">
-            <TitleSidebar title='หมวดหมู่' />
-
-            {sidebarMenu.map((item) =>{
-                return <ButtonSidebar key={item.path} section={item.section} path={item.path} status={item.path===namePath? true:false} /> 
-            })}
+        <aside  className="flex flex-col gap-15 left-0 shrink-0 sm:w-[260px] pt-[var(--start-top)] pl-[var(--start-left)] min-h-screen bg-black relative z-99 ">
+            <div className="flex flex-col gap-2 shrink-0" >
+                <TitleSidebar title='หมวดหมู่' />
+                {sidebarMenu.map((item) =>{
+                    return <ButtonSidebar key={item.path} section={item.section} path={item.path} status={item.path===namePath? true:false} /> 
+                })}
+            </div>
+            <div className="flex flex-col gap-5 shrink-0 pr-0.5" >
+                <TitleSidebar title="อนิเมะมาแรง" />
+                {topAnimeList.map((anime) => <CardTopAnime title={anime.title} watch={anime.watch} path={anime.path} key={uuidv4()} />)}
+            </div>
 
         </aside>
     );
