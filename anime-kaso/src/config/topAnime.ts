52636bc0c7054d901e5483065e02127fb878f57b
kawji <24.supavich@gmail.com>
2025-08-02 22:53:15 +0700

    config data top anime

---
+++ anime-kaso/src/config/topAnime.ts
@@ -0,0 +1,13 @@
+
+
+export const topAnimeList = [
+    {title:"Demon Slayer- Kimetsu no Yaiba Season 1" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"My Hero Academia FINAL SEASON" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"Solo Leveling Season 1" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"Solo Leveling Season 1" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"Solo Leveling Season 1 Yaiba Season 1" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"My Hero Academia FINAL SEASON" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+    {title:"Demon Slayer- Kimetsu no Yaiba Season 1" ,watch:'2.4 K' ,path:'anime-test.jpg' },
+
+
+]
