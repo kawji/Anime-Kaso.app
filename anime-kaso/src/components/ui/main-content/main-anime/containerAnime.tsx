daa4c665d34522c086c6a6f4fc5d95c67c89447e
kawji <24.supavich@gmail.com>
2025-08-03 14:51:04 +0700

    upgrad for responsive

--- anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function ContainerAnime({ children }:Props ) {
     return(
-        <article className="max-w-[1200px]  h-auto grid grid-cols-[repeat(auto-fit,minmax(160px,220px))] lg:gap-3 gap-2 " >
+        <article className="max-w-[70vw] p-0 w-[88%] lg:ml-[80px] sm:ml-[45px] ml-4  rounded-2xl h-auto flex flex-wrap lg:gap-4 gap-2 " >
             {children}
         </article>
     );
