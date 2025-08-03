06c13faecfcec368458ecd0306f3a81f34a8df79
kawji <24.supavich@gmail.com>
2025-08-03 14:51:50 +0700

    upgrad card top anime

--- anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
+++ anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
@@ -9,10 +9,10 @@ type Props = {
 export default function CardTopAnime({ title ,watch ,path }:Props) {
     const fullPath = `/top-anime/${path}`
     return(
-        <div className=" flex gap-2 rounded-lg group cursor-pointer " >
-            <img src={fullPath} alt="anime top" className="aspect-[3/4] w-[120px] h-full object-cover rounded-md " />
+        <div className=" flex gap-2 rounded-lg group cursor-pointer mb-2 " >
+            <img src={fullPath} alt="anime top" className="aspect-[3/4] w-[90px] h-full object-cover rounded-md " />
             <div className="flex flex-col gap-3 w-full lg:w-18" >
-                <p className=" font-medium text-[14px] line-clamp-5 group-hover:text-white/70 transition-all "> {title} </p>
+                <p className=" font-medium text-[13px] line-clamp-5 group-hover:text-white/70 transition-all "> {title} </p>
                 <span className="px-0 py-0.5 text-[13px] flex items-center justify-center border border-white/75 bg-[#212020] rounded-md gap-2 transition group-hover:text-white/70 " >
                     <Eye size={21} />
                     {watch}
