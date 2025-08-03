c13cd10087afe6cdfabfac2aec38a6bc68d87be3
kawji <24.supavich@gmail.com>
2025-08-03 14:51:35 +0700

    upgrad hover animation

--- anime-kaso/src/components/ui/Sidebar/button.tsx
+++ anime-kaso/src/components/ui/Sidebar/button.tsx
@@ -10,7 +10,7 @@ type Props ={
 export default function ButtonSidebar({ section, path, status }:Props ) {
     return(
         <div className="w-full h-auto flex items-center ">
-            <Link className={clsx('px-2 py-1 rounded-md text-[14px] font-light leading-relaxed hover:bg-white/15 cursor-pointer',status&&'bg-white/20')} href={path} >
+            <Link className={clsx('px-2 py-1 rounded-md text-[14px] font-light leading-relaxed hover:bg-white/15 cursor-pointer',status?'bg-white/20 text-white/95':'bg-white/0 text-white/65')} href={path} >
                 {section}
             </Link>
         </div>
