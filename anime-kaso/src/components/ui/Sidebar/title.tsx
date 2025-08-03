83147e9bc1f5ec0808909f3bddfa077779cc019b
kawji <24.supavich@gmail.com>
2025-08-03 14:52:02 +0700

    edit title anime

--- anime-kaso/src/components/ui/Sidebar/title.tsx
+++ anime-kaso/src/components/ui/Sidebar/title.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function TitleSidebar({title}:Props) {
     return(
         <section className="w-full h-auto flex items-center ">
-            <span className="px-2 py-1 rounded-md text-[14px] text-white/50 font-light leading-relaxed ">
+            <span className="px-2 py-1 rounded-md text-[14px] lg:text-2xl text-white/90 font-light leading-relaxed ">
                 {title}
             </span>
         </section>
