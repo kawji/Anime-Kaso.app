b62d436dcaa1394080c5217b022b9c6604af8389
kawji <24.supavich@gmail.com>
2025-08-03 14:51:18 +0700

    upgrad title anime

--- anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function TitleContent({ title }:Props ) {
     return(
-        <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold ml-[var(--start-left)] ">
+        <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold lg:ml-[80px] sm:ml-[45px] ml-4 ">
             {title}
         </h3>
     );
