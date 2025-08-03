ad7e7a9b618c72f3cb681b2edef1e0bf6cb01d1d
kawji <24.supavich@gmail.com>
2025-08-03 23:20:35 +0700

    set margin

--- anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function TitleContent({ title }:Props ) {
     return(
-        <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold lg:ml-[80px] sm:ml-[45px] ml-4 ">
+        <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold  ">
             {title}
         </h3>
     );
