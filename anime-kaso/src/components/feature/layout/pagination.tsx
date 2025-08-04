ec1dec944ebdc4ab97fd34a50d96283ea685e833
kawji <24.supavich@gmail.com>
2025-08-04 21:51:42 +0700

    set pagination

--- anime-kaso/src/components/feature/layout/pagination.tsx
+++ anime-kaso/src/components/feature/layout/pagination.tsx
@@ -1,18 +1,21 @@
 'use client'
 import { useSearchParams } from "next/navigation";
 import ButtonPagination from "@/components/ui/pagination/buttonPagi";
+import { useEffect, useState } from "react";
 
 
 export default function Pagination() {
     const searchParams = useSearchParams();
-    const page = Number( searchParams.get('page') || 1 )
+    const [page ,setPage] = useState(1);
+
+    useEffect(() => {
+        setPage(Number(searchParams.get('page') || 1))
+    },[searchParams])
     
     return(
         <nav className="w-auto h-auto lg:h-15 bg-gray-400/25 flex items-center gap-4 px-6 py-4 rounded-md  " >
-            <ButtonPagination page={1} />
-            <ButtonPagination page={2} />
-            <ButtonPagination page={3} />
-            <ButtonPagination page={4} />
+            {[page,page+1,page+2,page+3].map((item)=><ButtonPagination page={item} pageCurrent={page} /> )}
+            
 
         </nav>
     );
