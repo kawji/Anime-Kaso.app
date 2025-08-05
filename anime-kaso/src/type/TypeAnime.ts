ebefe129784e9e8bfd45d93a1905b7227b3a2024
kawji <24.supavich@gmail.com>
2025-08-05 22:31:51 +0700

    chang name to TypeAnime

---
+++ anime-kaso/src/type/TypeAnime.ts
@@ -0,0 +1,19 @@
+
+
+
+export type TypeAnime = {
+    mal_id:number;
+    title: string;
+    images: {
+        jpg: {
+        image_url: string;
+        };
+    };
+    favorites: number;
+    popularity: number;
+
+
+
+
+
+}
