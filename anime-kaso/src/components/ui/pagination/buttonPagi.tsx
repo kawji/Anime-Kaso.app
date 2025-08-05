2a84a2c7925bcaea57a9afdd5216c0d09b5b8aff
kawji <24.supavich@gmail.com>
2025-08-05 22:29:26 +0700

    show content pagination button

--- anime-kaso/src/components/ui/pagination/buttonPagi.tsx
+++ anime-kaso/src/components/ui/pagination/buttonPagi.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter ,useSearchParams } from "next/navigation";
 import clsx from "clsx";
+import { useState } from "react";
 
 type Props = {
     page:number;
@@ -9,16 +10,18 @@ type Props = {
 
 export default function ButtonPagination({ page ,pageCurrent }:Props) {
     const router = useRouter();
+    const [show ,setShow] = useState(true);
 
     const gotoPage = () => {
         router.push(`/anime?page=${page}`);
     }
 
+
     return(
-        <button onClick={gotoPage} className={clsx("px-4 py-2.5 rounded-sm bg-[#161515] text-sm cursor-pointer hover:bg-white/60 hover:text-gray-900 transition-colors "
+        <button onClick={gotoPage} className={clsx("px-4 py-2.5 rounded-sm bg-[#161515] text-sm cursor-pointer hover:bg-white/40 hover:text-gray-900 transition-colors "
             ,pageCurrent===page? 'bg-white/80 text-black/85':'text-white/75' 
         )}>
-        {page}
+        {page >= pageCurrent ? page - 3 == pageCurrent? '>': page  : page+3 == pageCurrent ? '<' : page  }
     </button>
     );
 }
