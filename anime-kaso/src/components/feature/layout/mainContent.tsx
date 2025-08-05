38bcc825499887f73559ce514548a58a46d24433
kawji <24.supavich@gmail.com>
2025-08-05 22:27:31 +0700

    main content zone ubdate for pagination useing

--- anime-kaso/src/components/feature/layout/mainContent.tsx
+++ anime-kaso/src/components/feature/layout/mainContent.tsx
@@ -6,38 +6,31 @@ import CardAnime from "@/components/ui/main-content/main-anime/cardAnime";
 import ContainerTitle from "@/components/ui/main-content/main-anime/containerTitle";
 import CarouselAnime from "./carouselAnime";
 import Pagination from "./pagination";
-import { usePathname ,useSearchParams } from "next/navigation";
 import { useAnimeList } from "@/hooks/useAnimeList";
-import { useEffect, useState } from "react";
-import { animeType } from "@/type/anime";
+import { TypeAnime } from "@/type/TypeAnime";
+import { usePathPagination } from "@/hooks/usePathPagination";
 
 export default function MainContent() {
-    const pathName = usePathname();
-    const searchParams = useSearchParams()
-    const [page ,setPage] = useState<number>(1);
-    
-    useEffect(()=> {
-        const paginationPage = Number(searchParams.get('path') || 1)
-        setPage(paginationPage);
-    },[searchParams])
+    const {pageNumber ,pageName} = usePathPagination()
 
-    const {data: animeList, isLoading, isError } = useAnimeList(page);
+    const {data: animeList, isLoading, isError } = useAnimeList(pageNumber);
     
     console.log('loading ==>',isLoading);
     console.log('Error ==>',isError);
     console.log('Data Anime ==>',animeList);
-    console.log('Data Anime ==>',page);
+    console.log('Pages ==>',pageNumber);
+
 
 
 
     return(
         <main className="flex flex-col items-center flex-1 gap-0 lg:gap-5 w-full minh-h-screen pt-10 sm:pt-16 ">
-            { pathName==='/'&& <CarouselAnime />}
+            { pageName==='/'&& <CarouselAnime />}
             <ContainerTitle >
                 <TitleContent title="อนิเมะล่าสุด" />
             </ContainerTitle>
             <ContainerAnime>            
-                {animeList?.map((anime:animeType) => <CardAnime key={anime.mal_id} png={anime.images.jpg.image_url} title={anime.title} favorites={anime.favorites} />)}
+                {animeList?.map((anime:TypeAnime) => <CardAnime key={anime.mal_id} png={anime.images.jpg.image_url} title={anime.title} favorites={anime.favorites} />)}
             </ContainerAnime>
             <Pagination />
 
