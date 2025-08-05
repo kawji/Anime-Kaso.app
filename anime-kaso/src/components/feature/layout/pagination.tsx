8c078749b17c281d1d1a7d589cfe2352c1305ec8
kawji <24.supavich@gmail.com>
2025-08-05 22:27:53 +0700

    navbar update ui

--- anime-kaso/src/components/feature/layout/pagination.tsx
+++ anime-kaso/src/components/feature/layout/pagination.tsx
@@ -13,8 +13,9 @@ export default function Pagination() {
     },[searchParams])
     
     return(
-        <nav className="w-auto h-auto lg:h-15 bg-gray-400/25 flex items-center gap-4 px-6 py-4 rounded-md  " >
-            {[page,page+1,page+2,page+3].map((item)=><ButtonPagination page={item} pageCurrent={page} /> )}
+        <nav className="w-auto h-auto lg:h-15 bg-[#161515] border-white/25 border flex items-center gap-4 px-6 py-4 rounded-md  " >
+            
+            {[page-3,page-2,page-1,page,page+1,page+2,page+3,page+50].map((item)=><ButtonPagination page={item} pageCurrent={page} /> )}
             
 
         </nav>
