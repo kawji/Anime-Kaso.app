035eca7e506071dcb593bf0fcc4ddb9c558d385b
kawji <24.supavich@gmail.com>
2025-08-05 22:30:21 +0700

    set endpoint anime

--- anime-kaso/src/hooks/useAnimeList.ts
+++ anime-kaso/src/hooks/useAnimeList.ts
@@ -5,7 +5,7 @@ export function useAnimeList( page:number ) {
     return useQuery({
         queryKey:['anime',page],
         queryFn: async () => {
-            const { data } = await axios.get(`https://api.jikan.moe/v4/anime?page=${page}&limit=25`);
+            const { data } = await axios.get(`https://api.jikan.moe/v4/anime?order_by=start_date&sort=desc&page=${page}&limit=25`);
             return data.data
         },
         staleTime: 1000 * 60 * 5, // แคชไว้ 5 นาที
