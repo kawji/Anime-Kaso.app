10ea4a8705cd317621725bffb8c12bc780693c30
kawji <24.supavich@gmail.com>
2025-08-05 22:29:47 +0700

    show content card top anime

--- anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
+++ anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
@@ -2,19 +2,18 @@ import { Eye } from "lucide-react";
 
 type Props = {
     title:string;
-    watch:string;
-    path:string;
+    watch:number;
+    png:string;
 }
 
-export default function CardTopAnime({ title ,watch ,path }:Props) {
-    const fullPath = `/top-anime/${path}`
+export default function CardTopAnime({ title ,watch ,png }:Props) {
     return(
         <div className=" flex gap-2 rounded-lg group cursor-pointer mb-2 " >
-            <img src={fullPath} alt="anime top" className="aspect-[3/4] w-[90px] h-full object-cover rounded-md " />
+            <img src={png} alt="anime top" className="aspect-[3/4] w-[90px] h-full object-cover rounded-md " />
             <div className="flex flex-col gap-3 w-full lg:w-18" >
                 <p className=" font-medium text-[13px] line-clamp-5 group-hover:text-white/70 transition-all "> {title} </p>
-                <span className="px-0 py-0.5 text-[13px] flex items-center justify-center border border-white/75 bg-[#212020] rounded-md gap-2 transition group-hover:text-white/70 " >
-                    <Eye size={21} />
+                <span className="px-1 py-0.5 text-[13px] flex items-center justify-center border border-white/75 bg-[#212020] rounded-md gap-2 transition group-hover:text-white/70 " >
+                    <Eye size={21} className="shrink-0" />
                     {watch}
                 </span>
             </div>
