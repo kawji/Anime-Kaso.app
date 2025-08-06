d174dedb6dda73a7907d199ddf800ad3631fa0f5
kawji <24.supavich@gmail.com>
2025-08-06 23:40:30 +0700

    create loading component

---
+++ anime-kaso/src/components/ui/system/loading.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Loading() {
+    return(
+        <div  className="w-full h-full flex items-center justify-center">
+            <span className="animate-spin border-t-4 border-l-2 rounded-full h-12 w-12 border-blue-500  ">
+
+            </span>
+        </div>
+    );
+}
