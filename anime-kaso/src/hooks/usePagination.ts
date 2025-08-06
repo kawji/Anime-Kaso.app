bd2d37f5258065427104c5c269d10572f41c729d
kawji <24.supavich@gmail.com>
2025-08-06 23:41:30 +0700

    create cleancode of pagination

---
+++ anime-kaso/src/hooks/usePagination.ts
@@ -0,0 +1,31 @@
+'use client'
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useLastPagination } from "@/hooks/useLastPagination";
+import { viewPort } from "@/utils/viewport";
+
+export function usePagination() {
+        const searchParams = useSearchParams();
+        const [page ,setPage] = useState(1);
+        const { data ,isLoading ,isError } = useLastPagination(`https://api.jikan.moe/v4/anime?order_by=start_date&sort=desc&page=${page}&limit=25`);
+        const pageLast = data?.pagination?.last_visible_page ?? 1
+
+        const { oneMachine } = viewPort();
+
+
+        useEffect(() => {
+            setPage(Number(searchParams.get('page') || 1))
+        },[searchParams])
+
+        const pageList = [ page-4 ,page-3 ,page-2 ,page-1 ,page ,page+1 ,page+2 ,page+3 ,page+4 ,page+5  ].filter((p) => p >= 1 && p <= pageLast );
+
+
+        return {page  ,pageLast ,pageList }
+}
+
+// const handleResize = () => {
+//     const width = window.innerWidth;
+//     if (width < 480) setMaxVisiblePages(1);     // มือถือ
+//     else if (width < 768) setMaxVisiblePages(3); // tablet
+//     else setMaxVisiblePages(5);                  // desktop
+//   };
