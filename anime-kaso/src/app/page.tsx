135d170afdb675b651a0fcf3ae552ccc80dad374
Kaw_Ji <24.supavich@gmail.com>
2025-08-09 15:29:27 +0700

    css update

--- anime-kaso/src/app/page.tsx
+++ anime-kaso/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Sidebar from "@/components/feature/layout/sidebar";
 import MainContent from "@/components/feature/layout/mainContent";
 
+
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen pb-20 flex overflow-x-hidden " >
+    <div className="font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black " >
       <Sidebar />
       <MainContent />
     </div>
