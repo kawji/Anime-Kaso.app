eb3a95d36087b04d857e49a089ac9fcc4302ce85
Kaw_Ji <24.supavich@gmail.com>
2025-08-09 15:28:36 +0700

    tone web

--- anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
+++ anime-kaso/src/components/ui/sidebar/cardTopAnime.tsx
@@ -10,7 +10,7 @@ export default function CardTopAnime({ title ,watch ,png }:Props) {
     return(
         <div className=" flex gap-2 rounded-lg group cursor-pointer mb-2 " >
             <img src={png} alt="anime top" className="aspect-[3/4] w-[90px] h-full object-cover rounded-md " />
-            <div className="flex flex-col gap-3 w-full lg:w-18" >
+            <div className="flex flex-col gap-3 w-full lg:w-18 text-white" >
                 <p className=" font-medium text-[13px] line-clamp-5 group-hover:text-white/70 transition-all "> {title} </p>
                 <span className="px-1 py-0.5 text-[13px] flex items-center justify-center border border-white/75 bg-[#212020] rounded-md gap-2 transition group-hover:text-white/70 " >
                     <Eye size={21} className="shrink-0" />
