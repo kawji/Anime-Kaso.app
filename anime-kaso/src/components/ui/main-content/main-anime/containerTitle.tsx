2fc4dcbefc9c520c9e4a1cbaf1e9177414df785e
Kaw_Ji <24.supavich@gmail.com>
2025-08-09 15:28:43 +0700

    tone css

--- anime-kaso/src/components/ui/main-content/main-anime/containerTitle.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/containerTitle.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function ContainerTitle({ children }:Props ) {
     return(
-        <div className="relative flex items-center justify-start lg:ml-[80px] mt-8 lg:mb-5 sm:ml-[45px] ml-4 max-w-[70vw] w-[88%] h-auto " >
+        <div className="relative flex items-center justify-start lg:ml-[0px] mt-8 lg:mb-0 sm:ml-[45px] ml-4 max-w-[70vw] w-[88%] h-auto leading-0 " >
             {children}
         </div>
     );
