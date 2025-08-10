106db16b34c90d4c5fb2b928e5519e013a0a2a79
Kaw_Ji <24.supavich@gmail.com>
2025-08-10 16:15:44 +0700

    new path

---
+++ anime-kaso/src/app/end/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/feature/layout/sidebar";
+import MainContent from "@/components/feature/layout/mainContent";
+
+export default function AnimeEnd() {
+  return (
+    <div className="font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black " >
+      <Sidebar />
+      <MainContent />
+    </div>
+  );
+}
