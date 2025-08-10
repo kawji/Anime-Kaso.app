db5189e95582056e14b85f2b151dc34280797252
Kaw_Ji <24.supavich@gmail.com>
2025-08-10 16:15:00 +0700

    set main content

--- anime-kaso/src/components/feature/layout/mainContent.tsx
+++ anime-kaso/src/components/feature/layout/mainContent.tsx
@@ -9,21 +9,17 @@ import Pagination from "./pagination";
 import { useAnimeList } from "@/hooks/useAnimeList";
 import { TypeAnime } from "@/type/TypeAnime";
 import { usePathPagination } from "@/hooks/usePathPagination";
-import Loading from "@/components/ui/system/spinner";
 import { Skeleton } from "@/components/ui/system/skeleton";
 
 export default function MainContent() {
     const {pageNumber ,pageName} = usePathPagination()
-
-    const {data: animeList, isLoading, isError } = useAnimeList(pageNumber);
+    console.log('Page Name ==>',pageName);
+    const {data: animeList, isLoading, isError } = useAnimeList(pageNumber ,pageName);
     
     console.log('loading ==>',isLoading);
     console.log('Error ==>',isError);
     console.log('Data Anime ==>',animeList);
     console.log('Pages ==>',pageNumber);
-
-
-
 
     return(
         <main className="flex flex-col items-center flex-1 gap-0 lg:gap-5 w-full minh-h-screen pt-10 sm:pt-16 bg-[#101010] ">
@@ -35,7 +31,6 @@ export default function MainContent() {
                 {isLoading? <Skeleton width="w-full" height="h-[725px]" rounded="rounded-lg" key='loadingAnimeList' /> : animeList?.data.map((anime:TypeAnime) => <CardAnime key={anime.mal_id} png={anime.images.jpg.image_url} title={anime.title} favorites={anime.favorites} />)}
             </ContainerAnime>
             {isLoading? <Skeleton width="w-[150px]" height="h-[25px]" rounded="rounded-md" key='loadingPagination' /> : <Pagination />}
-
 
         </main>
     );
