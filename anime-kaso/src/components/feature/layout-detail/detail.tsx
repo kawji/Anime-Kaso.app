6da3978d4d94fd101938832e2a07528ba972449d
Kaw_Ji <24.supavich@gmail.com>
2025-08-11 16:26:41 +0700

    new layout detail

---
+++ anime-kaso/src/components/feature/layout-detail/detail.tsx
@@ -0,0 +1,12 @@
+
+export default function Detail() {
+    
+    return(
+        <div className="flex w-full h-auto bg-blue-950/50 ">
+        <div className="" >
+            <img src='#' className="" />
+        </div>
+
+        </div>
+    );
+}
