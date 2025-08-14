69cd12a490e8bc8c7a9fa5b9ea76c4aba3793669
Kaw_Ji <24.supavich@gmail.com>
2025-08-14 17:31:12 +0700

    detail

--- anime-kaso/src/app/anime/[id]/page.tsx
+++ anime-kaso/src/app/anime/[id]/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
-
+import Detail from "@/components/feature/layout-detail/detail";
 export default function AnimeAll() {
     const { id } = useParams();
     useEffect(()=>{
         
     },[id])
+
     return (
-    <div className="font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black flex-col " >
+    <div className="mt-10 sm:mt-16 font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black/50  flex-col " >
+      <Detail />
+
 
     </div>
   );
