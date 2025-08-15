5c2bcd1b94732a470118f1bfd93c73adbd82c301
kawji <24.supavich@gmail.com>
2025-08-15 22:08:31 +0700

    new get anime

---
+++ anime-kaso/src/hooks/useGetDetail.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+
+
+export function useGetDetail(id: number | string) {
+    return useQuery({
+        queryKey: ['animeDetail', id],
+        enabled: !!id,
+        queryFn: async () => {
+            const num = Number(id);
+            const { data } = await axios.get(`https://api.jikan.moe/v4/anime/${num}`);
+            return data.data;
+        },
+        staleTime: 1000 * 60 * 5,
+    })
+}
