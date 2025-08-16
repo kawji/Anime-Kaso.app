6d43cd506519f28e018ff068a1aaeec31aa734d9
kawji <24.supavich@gmail.com>
2025-08-16 20:59:58 +0700

    create icon

---
+++ anime-kaso/src/components/ui/icons/bilibili.tsx
@@ -0,0 +1,8 @@
+
+import { Icon } from '@iconify/react';
+
+export default function Bilibili() {
+    return(
+        <Icon icon="simple-icons:bilibili"  color="#00A1D6" width="40" height="40"  />
+    );
+}
