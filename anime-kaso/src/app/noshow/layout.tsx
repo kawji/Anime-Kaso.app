a0502c27ab2f10720a4e467a2873164659171778
kawji <24.supavich@gmail.com>
2025-08-16 22:04:38 +0700

    chang name page

---
+++ anime-kaso/src/app/noshow/layout.tsx
@@ -0,0 +1,14 @@
+'use client'
+import "../globals.css";
+import { QueryClient } from '@tanstack/react-query';
+import { useState } from "react";
+
+export default function NoShow({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return children
+}
