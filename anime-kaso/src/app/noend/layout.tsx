519eeb59754287cc7522bcc393e70769ab35fc73
kawji <24.supavich@gmail.com>
2025-08-16 22:04:33 +0700

    chang name page

--- anime-kaso/src/app/noend/layout.tsx
+++ anime-kaso/src/app/noend/layout.tsx
@@ -1,11 +1,9 @@
 'use client'
 import "../globals.css";
-import Navbar from "@/components/feature/layout/navbar";
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { useState } from "react";
 
-export default function AnimeNoEnd({
+export default function NoEnd({
   children,
 }: Readonly<{
   children: React.ReactNode;
