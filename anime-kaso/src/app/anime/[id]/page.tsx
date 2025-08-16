4a1ea063969ea58ca03f1098a709fd74cb46fc7f
kawji <24.supavich@gmail.com>
2025-08-16 22:03:42 +0700

    update page

--- anime-kaso/src/app/anime/[id]/page.tsx
+++ anime-kaso/src/app/anime/[id]/page.tsx
@@ -2,22 +2,26 @@
 import Anime from "@/components/feature/layout-detail/anime";
 import Watch from "@/components/feature/layout-detail/watch";
 type AnimeProps = {
-  params: {
+  params: Promise<{
     id:string
-  }
+  }>
 }
 
-export default async function AnimeAllDetail({ params }:AnimeProps ) {
-  const { id } = await params
-    console.log('id path',params.id)
-
+export default async function AnimeAllDetail(props:AnimeProps) {
+  const params = await props?.params;
+  console.log('id path',params)
+    // ป้องกันตั้งแต่ต้น ถ้า id ไม่ถูกต้อง
+  if (!params.id || isNaN(Number(params.id))) {
+        return <div>Invalid Anime ID</div>; // หรือ redirect, notFound() ก็ได้
+  }else {
     return (
-    <div className="mt-5 sm:mt-7 font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black/50  flex-col " >
-      <Anime id={id}  />
-      <Watch />
-
-    </div>
-  );
+      <div className="mt-5 sm:mt-7 font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black/50  flex-col " >
+        <Anime id={params.id==='' ? Number('52480'):params.id}  />
+        <Watch />
+    
+      </div>
+    ); 
+  }
 }
 
 // 'https://api.jikan.moe/v4/anime/${id}'
