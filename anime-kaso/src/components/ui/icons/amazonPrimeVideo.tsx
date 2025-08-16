dd06055c00ad7776145ad534f30ba3fb2a9ab12f
kawji <24.supavich@gmail.com>
2025-08-16 20:59:54 +0700

    create icon

---
+++ anime-kaso/src/components/ui/icons/amazonPrimeVideo.tsx
@@ -0,0 +1,8 @@
+
+import { Icon } from '@iconify/react';
+
+export default function AmazonPrimeVideo() {
+    return(
+        <Icon icon="simple-icons:amazonprime"  color="#00A1D6" width="40" height="40"  />
+    );
+}
