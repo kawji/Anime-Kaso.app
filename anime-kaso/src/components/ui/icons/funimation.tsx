ddf8120d5bf18ac044b5590662dfc20c490ae558
kawji <24.supavich@gmail.com>
2025-08-16 21:00:06 +0700

    create icon

---
+++ anime-kaso/src/components/ui/icons/funimation.tsx
@@ -0,0 +1,7 @@
+import { Icon } from '@iconify/react';
+
+export default function Funimation() {
+    return(
+        <Icon icon="simple-icons:funimation"  color="#ffff" width="40" height="40"  />
+    );
+}
