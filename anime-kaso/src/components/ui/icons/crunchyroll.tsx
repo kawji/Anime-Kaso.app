dc9c9eaf42a9c8229c7b3c53b50a843528521e74
kawji <24.supavich@gmail.com>
2025-08-16 21:00:02 +0700

    create icon

---
+++ anime-kaso/src/components/ui/icons/crunchyroll.tsx
@@ -0,0 +1,7 @@
+import { Icon } from '@iconify/react';
+
+export default function Crunchyroll() {
+    return(
+        <Icon icon="simple-icons:crunchyroll"  color="#FF5E00" width="40" height="40"  />
+    );
+}
