476f031a0e8519f142dd06873ae859ba3d0e258d
kawji <24.supavich@gmail.com>
2025-08-16 22:05:47 +0700

    พยายามแก้ บัก Error syntax API จากไหนก้ไม่รุ้

--- anime-kaso/src/config/sidebarMenu.ts
+++ anime-kaso/src/config/sidebarMenu.ts
@@ -3,7 +3,7 @@ export const sidebarMenu = [
     {section:'อนิเมะทั้งหมด', path:'/anime'},
     {section:'กำลังฉาย', path:'/show'},
     {section:'ยังไม่ฉาย', path:'/noshow'},
-    {section:'อนิเมะไม่ลามุก', path:'/animehentai'},
+    {section:'อนิเมะไม่ 18+', path:'/animeNoHentei'},
     {section:'อนิเมะ - Hentai', path:'/animehentai'},
     {section:'อนิเมะ - Movie', path:'/animemovie'},
     {section:'ยังไม่จบ', path:'/noend'},
