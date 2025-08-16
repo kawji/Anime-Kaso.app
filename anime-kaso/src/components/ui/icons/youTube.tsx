8c23b9aaaeeee4f39aeb884330c0ef19aa264eb3
kawji <24.supavich@gmail.com>
2025-08-16 21:00:16 +0700

    create icon

---
+++ anime-kaso/src/components/ui/icons/youTube.tsx
@@ -0,0 +1,8 @@
+
+import { Icon } from '@iconify/react';
+
+export default function YouTube() {
+    return(
+        <Icon icon="simple-icons:youtube"  color="#FF0000" width="40" height="40"  />
+    );
+}
