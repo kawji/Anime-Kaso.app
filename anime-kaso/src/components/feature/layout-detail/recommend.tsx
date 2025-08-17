fa5d25105634839f3ed521e298da1845fffe7f93
kawji <24.supavich@gmail.com>
2025-08-17 17:25:16 +0700

    Create recommend.tsx

---
+++ anime-kaso/src/components/feature/layout-detail/recommend.tsx
@@ -0,0 +1,10 @@
+import CarouselScroll from "@/components/ui/detail-anime/carousel";
+
+export default function Recommend() {
+    return(
+        <div className="flex flex-col gap-5 w-full pl-[40px] ">
+            <h2 className="text-white/95 font-bold text-[26px]  ">อนิเมะที่คุณอาจสนใจ</h2>
+            <CarouselScroll />
+        </div>
+    );
+}
