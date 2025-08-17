91b7863d85ab09fb40659b8c96f4092c03d816cc
kawji <24.supavich@gmail.com>
2025-08-17 21:54:12 +0700

    Update titleContent.tsx

--- anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/titleContent.tsx
@@ -4,8 +4,12 @@ type Props = {
 
 export default function TitleContent({ title }:Props ) {
     return(
-        <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold  tracking-wider ">
-            {title}
-        </h3>
+        <div className="mt-2 flex flex-col gap-5 items-start">
+            <h3 className="text-2xl lg:text-4xl text-white/90 font-semibold  tracking-wider ">
+                {title}
+            </h3>
+            <p className="text-[18px] text-white/70 font-light tracking-wide ">รายการอนิเมะที่เเนะนำ</p>
+        </div>
+
     );
 }
