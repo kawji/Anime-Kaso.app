1a9cf290374e97bd7f7d657dad2bb9e8d6dad502
kawji <24.supavich@gmail.com>
2025-08-17 21:53:53 +0700

    Create footer.tsx

---
+++ anime-kaso/src/components/feature/layout-detail/footer.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Footer() {
+    return(
+        <div className="w-full flex flex-col items-center h-auto p-[40px] bg-amber-50/2 gap-5 ">
+            <h1 className="flex w-full leading-relaxed text-2xl text-white items-center justify-center font-light tracking-wide ">อนิเมะถูกลิขสิทธิ์มากมาย</h1>
+            <h1 className="flex w-full leading-relaxed text-[18px] text-white/70 items-center justify-center font-light tracking-wider ">รองรับแพลตฟอร์มต่างๆมากมายให้รับชม</h1>
+                
+        </div>
+    );
+}
