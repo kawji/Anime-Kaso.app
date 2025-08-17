66c325c13ab3a10ec56e827bb7a93e1c95c6db93
kawji <24.supavich@gmail.com>
2025-08-17 21:54:10 +0700

    Update containerAnime.tsx

--- anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
@@ -5,8 +5,9 @@ type Props = {
 
 export default function ContainerAnime({ children }:Props ) {
     return(
-        <article className="max-w-[70vw] lg:min-h-50 p-8 w-[88%] ml-[0px] bg-[#161515] shadow-white/30 border-white/20 border rounded-2xl h-auto flex flex-wrap lg:gap-4 gap-2 " >
+        <article className="mt-3 max-w-[70vw] lg:min-h-50 w-[88%] ml-[0px]  h-auto flex flex-wrap lg:gap-4 gap-2 " >
             {children}
         </article>
     );
 }
+// p-8 bg-[#161515] shadow-white/30 border-white/20 border rounded-2xl
