00737c9f0ebb5f23031379c52c73ddf05580a1c5
kawji <24.supavich@gmail.com>
2025-08-17 11:01:13 +0700

    Update useAnimePopula.ts

--- anime-kaso/src/hooks/useAnimePopula.ts
+++ anime-kaso/src/hooks/useAnimePopula.ts
@@ -2,11 +2,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-export function useAnimePopula() {
+export function useAnimePopula(limit:number) {
     return useQuery({
         queryKey:['popularity'],
         queryFn: async () => {
-            const response = await axios.get('https://api.jikan.moe/v4/top/anime?filter=bypopularity&limit=10');
+            const response = await axios.get(`https://api.jikan.moe/v4/top/anime?filter=bypopularity&limit=${limit}`);
             return response.data
         },
         staleTime: 1000 *60 *5 // cash 5 mi
