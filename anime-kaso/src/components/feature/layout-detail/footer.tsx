46ecda86f8fcd7febf96467d77e47d39a53abd25
kawji <24.supavich@gmail.com>
2025-08-20 21:44:55 +0700

    Update footer.tsx

--- anime-kaso/src/components/feature/layout-detail/footer.tsx
+++ anime-kaso/src/components/feature/layout-detail/footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
     return(
-        <div className="w-full flex flex-col items-center h-auto p-[40px] bg-amber-50/2 gap-5 ">
+        <div className="w-full flex flex-col items-center h-auto p-[40px] bg-amber-50/2 lg:gap-4 ">
             <h1 className="flex w-full leading-relaxed text-2xl text-white items-center justify-center font-light tracking-wide ">อนิเมะถูกลิขสิทธิ์มากมาย</h1>
             <h1 className="flex w-full leading-relaxed text-[18px] text-white/70 items-center justify-center font-light tracking-wider ">รองรับแพลตฟอร์มต่างๆมากมายให้รับชม</h1>
                 
