00d60e2bf3ae02b303e5823c56d1ee3ba37d7fd7
kawji <24.supavich@gmail.com>
2025-08-21 21:14:43 +0700

    Create mapIconsList.ts

---
+++ anime-kaso/src/config/mapIconsList.ts
@@ -0,0 +1,10 @@
+
+
+export const mapIconsList = {
+    netflix: { pathname:'Netflix' ,name:'Netflix' },
+    youtube: { pathname:'YouTube' ,name:'YouTube' },
+    amazon: { pathname:'AmazonPrimeVideo' ,name:'Amazon Prime Video' },
+    bilibili: { pathname:'Bilibili' ,name:'Bilibili' },
+    crunchyroll: { pathname:'Crunchyroll' ,name:'Crunchyroll' },
+    funimation: { pathname:'Funimation' ,name:'Funimation' },
+}
