74e70f1a08759063da0f678d868e914fddd479d3
kawji <24.supavich@gmail.com>
2025-08-21 21:14:38 +0700

    Update imageAnime.tsx

--- anime-kaso/src/components/ui/detail-anime/imageAnime.tsx
+++ anime-kaso/src/components/ui/detail-anime/imageAnime.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
+import Spinner from "../system/spinner";
+import { Skeleton } from "../system/skeleton";
+
 
 type ImageAnimeProps = {
-    png: string;
+    png: string ;
 }
 
-export default function ImageAnime({ png}:ImageAnimeProps) {
+export default function ImageAnime({ png }:ImageAnimeProps) {
+    console.log('PNG Anime ---> ',png)
     return(
         <div className="aspect-[3/4]  min-w-[170px] max-w-[250px] w-full overflow-hidden flex items-center justify-center rounded-md  " >
-            <img src={png} className=" w-full h-full " />
+            {png?   <img src={png} className=" w-full h-full " loading="lazy" /> : <Skeleton width="w-full" height="h-full" rounded="rounded" /> }
         </div>
     );
 }
