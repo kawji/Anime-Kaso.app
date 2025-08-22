61aadc4128c90e00420570ef160600a05ce864ac
kawji <24.supavich@gmail.com>
2025-08-22 21:23:43 +0700

    Update anime.tsx

--- anime-kaso/src/components/feature/layout-detail/anime.tsx
+++ anime-kaso/src/components/feature/layout-detail/anime.tsx
@@ -2,13 +2,26 @@
 import { useGetDetail } from "@/hooks/useGetDetail";
 import ImageAnime from "@/components/ui/detail-anime/imageAnime";
 import DetailAnime from "@/components/ui/detail-anime/detail";
+import { animeData } from "@/config/animeData";
+import { useEffect } from "react";
+import { TypeDataAnime } from "@/config/animeData";
+
+
 type DetailProps = {
     id:string | number ;
 }
 export default function Anime({id}:DetailProps) {
-    const NumberId:number = Number(id);
-    const { data, isLoading ,isError } = useGetDetail(NumberId);
-    console.log('Data ---------====>',data)
+    const numberId:number = Number(id);
+    const { data, isLoading ,isError } = useGetDetail(numberId);
+    const detail = animeData[numberId]
+    
+    
+
+    useEffect(()=> {
+        if(detail) {
+            console.log('Detallll------------> ',detail)
+        }
+    },[detail])
 
     return(
         <div className="flex p-[40px]  mt-15 w-full h-auto bg-blue-950/0 gap-15 ">
