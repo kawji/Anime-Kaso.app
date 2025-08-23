dcc968ee44c5d5d947651e4cbac4cd25562e7bc9
kawji <24.supavich@gmail.com>
2025-08-23 22:41:44 +0700

    Create primevideo.tsx

---
+++ anime-kaso/src/components/ui/platform/primevideo.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Primevideo2() {
+    return(
+        <div>
+            <img src="/platform/Prime_Video_Logo.svg" alt="Logo" className="w-38 h-18 text-[#858383cd]" />
+        </div>
+    )
+}
