f875d97cb4cf1ee64301291c11c222dc1973a57f
kawji <24.supavich@gmail.com>
2025-08-23 22:41:42 +0700

    Create netflix2.tsx

---
+++ anime-kaso/src/components/ui/platform/netflix2.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Netflix2() {
+    return(
+        <div className="flex items-center" >
+            <img src="/platform/Netflix_Logo.svg" alt="Logo" className="w-25 h-10 text-[#858383cd]" />
+        </div>
+    )
+}
