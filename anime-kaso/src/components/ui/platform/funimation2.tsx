bb2e2231fd37721f2608557d8725945b3df9a869
kawji <24.supavich@gmail.com>
2025-08-23 22:41:36 +0700

    Create funimation2.tsx

---
+++ anime-kaso/src/components/ui/platform/funimation2.tsx
@@ -0,0 +1,8 @@
+
+export default function Funimation2() {
+    return(
+        <div>
+            <img src="/platform/Funimation_Logo.svg" alt="Logo" className="w-38 h-18 text-[#858383cd]" />
+        </div>
+    )
+}
