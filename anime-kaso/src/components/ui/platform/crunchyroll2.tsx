ec9ebfc14081a32bac84cefa7c61fd83662b7546
kawji <24.supavich@gmail.com>
2025-08-23 22:41:30 +0700

    Create crunchyroll2.tsx

---
+++ anime-kaso/src/components/ui/platform/crunchyroll2.tsx
@@ -0,0 +1,13 @@
+
+
+type PropsCrunchyroll2 = {
+    color:string
+}
+
+export default function Crunchyroll2({color}:PropsCrunchyroll2) {
+    return(
+        <div>
+            <img src="/platform/Crunchyroll_Logo.svg" alt="Logo" className="w-18 h-18 text-[#858383cd]" />
+        </div>
+    )
+}
