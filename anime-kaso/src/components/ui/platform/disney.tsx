dacf8915cc70d7885525244922b74fac2c45bf8c
kawji <24.supavich@gmail.com>
2025-08-23 22:41:34 +0700

    Create disney.tsx

---
+++ anime-kaso/src/components/ui/platform/disney.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Disney2() {
+    return(
+        <div className="flex items-center">
+            <img src="/platform/Disney_Logo.svg" alt="Logo" className="w-38 h-18 text-[#858383cd]"  />
+        </div>
+    )
+}
