d3f1cab10b00315df21ae115ae8da7c43aae0837
kawji <24.supavich@gmail.com>
2025-08-23 22:41:46 +0700

    Create youtube.tsx

---
+++ anime-kaso/src/components/ui/platform/youtube.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function YouTube2() {
+    return(
+        <div className="flex items-center">
+            <img src="/platform/youtube_Logo.svg" alt="Logo" className="w-38 h-18 text-[#858383cd]"  />
+        </div>
+    )
+}
