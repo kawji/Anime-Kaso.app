9f517992276c444e1f0b1132c3898c4e709f6d0d
kawji <24.supavich@gmail.com>
2025-08-23 09:45:00 +0700

    Update mapPlatformList.ts

--- anime-kaso/src/config/mapPlatformList.ts
+++ anime-kaso/src/config/mapPlatformList.ts
@@ -1,15 +1,27 @@
-export type ObjWatch = {
-    
+import Netflix from "@/components/ui/icons/netflix";
+import YouTube from "@/components/ui/icons/youTube";
+import AmazonPrimeVideo from "@/components/ui/icons/amazonPrimeVideo";
+import Bilibili from "@/components/ui/icons/bilibili";
+import Crunchyroll from "@/components/ui/icons/crunchyroll";
+import Funimation from "@/components/ui/icons/funimation";
+import { JSX } from "react";
+
+
+export type ObjPlatform = {
+    pathname: React.ComponentType;
+    name: string;
 }
 
-export const mapPlatformList = {
-    netflix: { pathname:'Netflix' ,name:'Netflix' },
-    youtube: { pathname:'YouTube' ,name:'YouTube' },
-    amazon: { pathname:'AmazonPrimeVideo' ,name:'Amazon Prime Video' },
-    bilibili: { pathname:'Bilibili' ,name:'Bilibili' },
-    crunchyroll: { pathname:'Crunchyroll' ,name:'Crunchyroll' },
-    funimation: { pathname:'Funimation' ,name:'Funimation' },
+export type recordPlatform = Record<string,ObjPlatform>
+
+export const mapPlatform:recordPlatform = {
+    netflix: { pathname: Netflix ,name:'Netflix' },
+    youtube: { pathname:YouTube ,name:'YouTube' },
+    amazon: { pathname:AmazonPrimeVideo ,name:'Amazon Prime Video' },
+    bilibili: { pathname:Bilibili ,name:'Bilibili' },
+    crunchyroll: { pathname:Crunchyroll ,name:'Crunchyroll' },
+    funimation: { pathname:Funimation ,name:'Funimation' },
 } as const;
 
-export type KeyPlatformList = keyof typeof mapPlatformList
+export type KeyPlatformList = keyof typeof mapPlatform
 
