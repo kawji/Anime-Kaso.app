b7327f970123f6e217026b0ed318613a28ef8dd9
kawji <24.supavich@gmail.com>
2025-08-23 22:41:15 +0700

    Update watch.tsx

--- anime-kaso/src/components/feature/layout-detail/watch.tsx
+++ anime-kaso/src/components/feature/layout-detail/watch.tsx
@@ -23,7 +23,7 @@ export default function Watch({ id }:PropsWatch) {
         <div className="w-full h-auto flex flex-col bg-blue-800/0 p-[40px] gap-5 ">
             <h1 className="flex items-center justify-start text-[26px] font-bold ">ช่องทางการรับชม</h1>
             <div className="flex items-center w-full h-auto py-5 pr-5  " > 
-                <div className={clsx("flex p-0 bg-white/0 gap-5 w-full h-auto " ,platformList.length > 0 ? '' : 'justify-center items-center')} >
+                <div className={clsx("flex p-0 bg-white/0 gap-5 w-full h-auto lg:max-h-[150px] " ,platformList.length > 0 ? '' : 'justify-center items-center')} >
                 {platformList.length > 0 ?  platformList?.map((platform) => <IconWatch tagIcon={<platform.pathname />} name={platform.name} />) : <h2 className="text-white/75 text-lg font-light leading-relaxed tracking-wide ">ไม่พบช่องทางการรับชม</h2>  }
 
 
