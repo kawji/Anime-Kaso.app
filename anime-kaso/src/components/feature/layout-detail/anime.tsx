7309c351c5e790aa973837328c1a7a94bf96faf3
kawji <24.supavich@gmail.com>
2025-08-23 09:44:52 +0700

    Update anime.tsx

--- anime-kaso/src/components/feature/layout-detail/anime.tsx
+++ anime-kaso/src/components/feature/layout-detail/anime.tsx
@@ -2,7 +2,7 @@
 import { useGetDetail } from "@/hooks/useGetDetail";
 import ImageAnime from "@/components/ui/detail-anime/imageAnime";
 import DetailAnime from "@/components/ui/detail-anime/detail";
-import { animeData } from "@/config/animeData";
+import { AnimeData } from "@/config/animeData";
 import { useEffect } from "react";
 import { TypeDataAnime } from "@/config/animeData";
 
@@ -13,7 +13,7 @@ type DetailProps = {
 export default function Anime({id}:DetailProps) {
     const numberId:number = Number(id);
     const { data, isLoading ,isError } = useGetDetail(numberId);
-    const detail = animeData[numberId]
+    const detail = AnimeData[numberId]
     
     
 
@@ -26,7 +26,7 @@ export default function Anime({id}:DetailProps) {
     return(
         <div className="flex p-[40px]  mt-15 w-full h-auto bg-blue-950/0 gap-15 ">
             <ImageAnime png={data?.images.jpg.image_url?? ''} />
-            <DetailAnime title={data?.title?? 'Anime Loading....'}   />
+            <DetailAnime title={data?.title?? 'Anime Loading....'} season={detail?.season || null } sub_title={detail?.subtitleList || null} discliption={detail?.resume || null} />
 
         </div>
     );
