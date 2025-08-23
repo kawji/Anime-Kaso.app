c4d981c8eca60073dd9c443197ac9642fd4f113d
kawji <24.supavich@gmail.com>
2025-08-23 09:44:55 +0700

    Update detail.tsx

--- anime-kaso/src/components/ui/detail-anime/detail.tsx
+++ anime-kaso/src/components/ui/detail-anime/detail.tsx
@@ -3,21 +3,19 @@ import { Tv } from "lucide-react";
 
 type DetailProps = {
     title:string;
-    sub_title?:string;
-    discliption?:string;
+    season:string | null;
+    sub_title:string[] | null;
+    discliption:string | null;
 }
 
-export default function DetailAnime({title ,sub_title ,discliption}:DetailProps) {
+export default function DetailAnime({title ,season ,sub_title ,discliption}:DetailProps) {
     return(
         
         <div className="flex-1 shrink-0 pt-5 w-full max-w-[60vw] bg-black/0 flex flex-col gap-1.5 " >
         <h1 className="lg:text-3xl font-bold  " >{title}</h1>
-        <h4 className=" lg:text-2xl font-bold text-white/50" >{sub_title?? 'Season  1'}</h4>
+        <h4 className=" lg:text-2xl font-bold text-white/50" >{season?? 'Season ---'}</h4>
         <div  className="flex gap-5 justify-start items-center h-auto w-auto mt-3 ">
-            <span className="flex justify-center items-center px-4 py-1 text-[13px] text-white/95 rounded-sm border-1 border-white/50 bg-white/5 " >ซับไทย</span>
-            <span className="flex justify-center items-center px-4 py-1 text-[13px] text-white/95 rounded-sm border-1 border-white/50 bg-white/5 " >ซับไทย</span>
-            <span className="flex justify-center items-center px-4 py-1 text-[13px] text-white/95 rounded-sm border-1 border-white/50 bg-white/5 " >ซับไทย</span>
-
+            {sub_title?.map((sub) =>  <span className="flex justify-center items-center px-4 py-1 text-[13px] text-white/95 rounded-sm border-1 border-white/50 bg-white/5 " > {sub} </span>)}
         </div>
         <div>
             <button className=" cursor-pointer hover:bg-white/85 transition-colors px-10 py-2.5 rounded-full mt-7 bg-white text-black flex items-center gap-4 font-light lg:text-[17px] " >
