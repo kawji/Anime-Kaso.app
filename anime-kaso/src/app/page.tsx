d492eaadc05f8a668277010c26b7bb2efec13ef4
kawji <24.supavich@gmail.com>
2025-08-24 22:08:20 +0700

    Update page.tsx

--- anime-kaso/src/app/page.tsx
+++ anime-kaso/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Sidebar from "@/components/feature/layout-main/sidebar";
 import MainContent from "@/components/feature/layout-main/mainContent";
-
+import Footer from "@/components/feature/layout-footer/footer";
 
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen pb-20 flex overflow-x-hidden text-white bg-black " >
-      <Sidebar />
-      <MainContent />
+    <div className="font-sans flex flex-col min-h-screen gap-5 ">
+      <div className=" flex overflow-x-hidden text-white bg-black pb-5 " >
+        <Sidebar />
+        <MainContent />
+      </div>
+      <Footer />
     </div>
   );
 }
