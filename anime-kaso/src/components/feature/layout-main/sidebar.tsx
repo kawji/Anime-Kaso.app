053267699e30c096236ebdca040b32ca5d5f0c0d
kawji <24.supavich@gmail.com>
2025-08-25 22:13:19 +0700

    Update sidebar.tsx

--- anime-kaso/src/components/feature/layout-main/sidebar.tsx
+++ anime-kaso/src/components/feature/layout-main/sidebar.tsx
@@ -13,14 +13,14 @@ export default function Sidebar() {
     const { data:animeList ,isLoading ,isError} = useAnimePopula(10);
 
     return(
-        <aside  className="flex flex-col gap-15 left-0 shrink-0 shadow-sm shadow-white/30 rounded-br-2xl sm:w-[260px] pt-[var(--start-top)] pl-[var(--start-left)] min-h-screen bg-[#131313] relative z-99 ">
-            <div className="flex flex-col gap-2 shrink-0" >
+        <aside  className=" p-5 lg:pb-0 lg:pl-10 flex flex-col gap-8 lg:gap-15 left-0 shrink-0 shadow-sm shadow-white/30 items-center lg:items-start w-full lg:w-[260px] lg:pt-[var(--start-top)] pl-[var(--start-left)] min-h-screen bg-[#131313] relative z-99 ">
+            <div className="flex flex-col items-start gap-2 shrink-0 w-full p-5 rounded-xl border border-white/20 lg:p-0 lg:border-none" >
                 <TitleSidebar title='หมวดหมู่' />
                 {sidebarMenu.map((item) =>{
                     return <ButtonSidebar key={item.path} section={item.section} path={item.path} status={item.path===namePath? true:false} /> 
                 })}
             </div>
-            <div className="flex flex-col gap-5 shrink-0 pr-0.5" >
+            <div className="flex flex-col gap-5 lg:gap-0 shrink-0 lg:pr-0.5 w-full items-start p-5 rounded-xl border border-white/20 lg:p-0 lg:border-none" >
                 <TitleSidebar title="อนิเมะมาแรง" />
                 {animeList?.data.map((anime:TypeAnime) => <CardTopAnime title={anime.title} watch={anime.favorites} png={anime.images.jpg.image_url} key={anime.mal_id} />)}
             </div>
