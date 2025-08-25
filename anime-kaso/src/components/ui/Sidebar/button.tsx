961b30230fab128a35689411d320464a8a5678db
kawji <24.supavich@gmail.com>
2025-08-25 22:13:28 +0700

    Update button.tsx

--- anime-kaso/src/components/ui/Sidebar/button.tsx
+++ anime-kaso/src/components/ui/Sidebar/button.tsx
@@ -9,8 +9,8 @@ type Props ={
 
 export default function ButtonSidebar({ section, path, status }:Props ) {
     return(
-        <div className="w-full h-auto flex items-center ">
-            <Link className={clsx('px-2 py-1 rounded-md text-[14px] font-light leading-relaxed hover:bg-white/15 cursor-pointer',status?'bg-white/20 text-white/95':'bg-white/0 text-white/65')} href={path} >
+        <div className="w-full h-auto flex justify-start ">
+            <Link className={clsx('lg:px-2 px-5 w-full lg:w-auto py-1 rounded-md text-md lg:text-[14px] font-light leading-relaxed hover:bg-white/15 cursor-pointer',status?'bg-white/20 text-white/95':'bg-white/0 text-white/65')} href={path} >
                 {section}
             </Link>
         </div>
