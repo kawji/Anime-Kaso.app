15c536574f35bb153d0d25634281fda83cd46b47
kawji <24.supavich@gmail.com>
2025-08-25 22:13:15 +0700

    Update footer.tsx

--- anime-kaso/src/components/feature/layout-footer/footer.tsx
+++ anime-kaso/src/components/feature/layout-footer/footer.tsx
@@ -3,7 +3,7 @@ import FooterAnime from "../layout-footer/footerAnime";
 
 export default function Footer() {
     return(
-        <footer className="w-full flex flex-col items-center h-auto pt-[40px] bg-amber-50/2 lg:gap-4 ">
+        <footer className="w-full flex flex-col items-center h-auto pt-[40px] bg-[#101010] lg:gap-4 ">
             <PlatformAnime />
             <FooterAnime />
         </footer>
