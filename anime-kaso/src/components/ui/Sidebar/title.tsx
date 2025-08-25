b4cc05f2d50d5796b0d71745d37ce448ef79b04c
kawji <24.supavich@gmail.com>
2025-08-25 22:13:32 +0700

    Update title.tsx

--- anime-kaso/src/components/ui/Sidebar/title.tsx
+++ anime-kaso/src/components/ui/Sidebar/title.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function TitleSidebar({title}:Props) {
     return(
         <section className="w-full h-auto flex items-center ">
-            <span className="px-2 py-1 rounded-md text-[14px] lg:text-2xl text-white/90 font-light leading-relaxed ">
+            <span className=" lg:px-2 px-5 lg:py-1 py-3 w-full mb-5 lg:mb-2 lg:w-auto border-b border-white/25 rounded-none lg:border-none lg:rounded-md text-lg font-medium lg:text-xl text-white/90 lg:font-light leading-relaxed ">
                 {title}
             </span>
         </section>
