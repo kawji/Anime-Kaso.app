c15969dbcb2daa0fc728a48ae0d48bbe3d12055d
kawji <24.supavich@gmail.com>
2025-08-26 21:22:24 +0700

    Update anime.tsx

--- anime-kaso/src/components/feature/layout-detail/anime.tsx
+++ anime-kaso/src/components/feature/layout-detail/anime.tsx
@@ -24,7 +24,8 @@ export default function Anime({id}:DetailProps) {
     },[detail])
 
     return(
-        <div className="flex p-[40px]  mt-15 w-full h-auto bg-blue-950/0 gap-15 ">
+        <div className="md:flex-row flex flex-col items-center p-[40px] mt-7 md:mt-15 w-full h-auto bg-blue-950/0 gap-7 md:gap-15 ">
+            <h1 className="sm:text-3xl text-2xl font-bold block md:hidden " >{data?.title?? 'Anime Loading....'}</h1>
             <ImageAnime png={data?.images.jpg.image_url?? ''} />
             <DetailAnime title={data?.title?? 'Anime Loading....'} season={detail?.season || null } sub_title={detail?.subtitleList || null} discliption={detail?.resume || null} />
 
