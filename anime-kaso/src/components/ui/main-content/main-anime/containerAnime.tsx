312c1780f8695b7ce875fd2d48f2da45930b12a7
kawji <24.supavich@gmail.com>
2025-08-26 21:22:51 +0700

    Update containerAnime.tsx

--- anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/containerAnime.tsx
@@ -5,7 +5,8 @@ type Props = {
 
 export default function ContainerAnime({ children }:Props ) {
     return(
-        <article className="mt-3 max-w-[70vw] w-[88%] ml-0 h-auto grid grid-cols-[repeat(auto-fill,minmax(170px,1fr))] gap-2 lg:gap-4" >
+        
+        <article className="mt-3 lg:max-w-[70vw] w-[95%] sm:w-[88%]  h-auto grid grid-cols-[repeat(auto-fill,minmax(80px,155px))]  sm:grid sm:grid-cols-[repeat(auto-fill,minmax(170px,1fr))] gap-4 sm:gap-2 lg:gap-4" >
             {children}
         </article>
     );
