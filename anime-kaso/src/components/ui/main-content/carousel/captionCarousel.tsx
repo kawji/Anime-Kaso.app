42d3eafcf981889a2f30bdebdf9b642c78a0ed25
kawji <24.supavich@gmail.com>
2025-08-26 21:22:41 +0700

    Update captionCarousel.tsx

--- anime-kaso/src/components/ui/main-content/carousel/captionCarousel.tsx
+++ anime-kaso/src/components/ui/main-content/carousel/captionCarousel.tsx
@@ -6,7 +6,7 @@ import { guideAnimeDetailType } from "@/type/guideAnime";
 
 export default function CaptionCarousel({ title ,subtitles ,chapter }:guideAnimeDetailType) {
     return(
-        <figcaption className="flex absolute right-0 bottom-0 w-[75%] h-auto py-0 bg-black/50 backdrop-blur-[4px] rounded-tl-2xl shadow-lg shadow-blue-500 " >
+        <figcaption className="flex absolute right-0 bottom-0 sm:w-[75%] w-full h-auto py-0 bg-black/50 backdrop-blur-[4px] sm:rounded-tl-2xl shadow-lg shadow-blue-500 " >
             <DetailCarousel title={title} subtitles={subtitles} chapter={chapter} />
             <ButtonCarousel />
         </figcaption>
