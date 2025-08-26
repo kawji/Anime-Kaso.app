d3999f1b5e9f8fc51b3c3261bc5551bc4fff919f
kawji <24.supavich@gmail.com>
2025-08-26 21:22:53 +0700

    Update containerTitle.tsx

--- anime-kaso/src/components/ui/main-content/main-anime/containerTitle.tsx
+++ anime-kaso/src/components/ui/main-content/main-anime/containerTitle.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function ContainerTitle({ children }:Props ) {
     return(
-        <div className="relative flex items-center justify-start lg:ml-[0px] mt-8 lg:mb-0 sm:ml-[45px] ml-4 max-w-[70vw] w-[88%] h-auto leading-0 " >
+        <div className="relative flex items-center justify-start  mt-8  ml-0 w-[95%] sm:w-[88%] lg:max-w-[70vw] lg:w-[88%] h-auto leading-0 " >
             {children}
         </div>
     );
