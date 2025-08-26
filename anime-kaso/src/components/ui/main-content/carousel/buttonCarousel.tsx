e4a8fdb99dd499578cc2c5f34570e683b9b7bcd7
kawji <24.supavich@gmail.com>
2025-08-26 21:22:39 +0700

    Update buttonCarousel.tsx

--- anime-kaso/src/components/ui/main-content/carousel/buttonCarousel.tsx
+++ anime-kaso/src/components/ui/main-content/carousel/buttonCarousel.tsx
@@ -5,7 +5,7 @@
 export default function ButtonCarousel() {
     return(
         <div className="p-8 flex justify-center items-end shrink-0 ">
-            <button className="px-9 py-[7px] bg-[var(--color-primary)] rounded-md text-black text-[16px] transition-all hover:bg-[#B0CAE2] hover:text-[var(--color-dense)] cursor-pointer ">รับชมตอนนี้</button>
+            <button className=" px-7.5 sm:px-9 py-[7px] sm:py-[7px] bg-[var(--color-primary)] rounded-md text-black text-[14px] sm:text-[16px] transition-all hover:bg-[#B0CAE2] hover:text-[var(--color-dense)] cursor-pointer ">รับชมตอนนี้</button>
         </div>
     );
 }
