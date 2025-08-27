c84b51970672005dc790081ab683fd7ae415e794
kawji <24.supavich@gmail.com>
2025-08-27 21:27:44 +0700

    Update iconWatch.tsx

--- anime-kaso/src/components/ui/detail-anime/iconWatch.tsx
+++ anime-kaso/src/components/ui/detail-anime/iconWatch.tsx
@@ -3,19 +3,19 @@ import React from "react"
 type PropsIconWatch = {
     tagIcon:React.ReactNode;
     name:string;
-
+    url:string;
 }
 
-export default function IconWatch({ tagIcon ,name }:PropsIconWatch ) {
+export default function IconWatch({ tagIcon ,name ,url }:PropsIconWatch ) {
 
     return(
-        <div className="shrink-0 group px-5 py-3 rounded-lg bg-[#141414] flex gap-3.5 cursor-pointer hover:bg-[#141414d5] transition-all hover:opacity-97  " >
+        <a className="shrink-0 group px-5 py-3 rounded-lg bg-[#141414] flex gap-3.5 cursor-pointer hover:bg-[#141414d5] transition-all hover:opacity-97  " href={url} target="_blank" >
             <span className="rounded-sm shrink-0 p-3 bg-white/7 group-hover:opacity-80 flex justify-center itemscenter ">{tagIcon}</span>
             <span className="p-2 flex flex-col items-start group-hover:opacity-97 ">
                 <h6 className="group-hover:opacity-97 group-hover:text-white/80 transition-all text-lg font-medium text-white/90 tracking-wider " >{name}</h6>
                 <p className="group-hover:opacity-97 group-hover:text-white/60 transition-all text-[13px] font-medium text-white/70 tracking-wider " >Watch in {name} </p>
             </span>
 
-        </div>
+        </a>
     )
 }
