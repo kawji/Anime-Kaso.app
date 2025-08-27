26068f4de85ba2ff3aedcf16c959d47623478d78
kawji <24.supavich@gmail.com>
2025-08-27 21:27:46 +0700

    Update netflix.tsx

--- anime-kaso/src/components/ui/icons/netflix.tsx
+++ anime-kaso/src/components/ui/icons/netflix.tsx
@@ -3,5 +3,6 @@ import { Icon } from '@iconify/react';
 export default function Netflix() {
     return(
         <Icon icon="simple-icons:netflix"  color="#E50914" width="40" height="40"  />
+        
     );
 }
