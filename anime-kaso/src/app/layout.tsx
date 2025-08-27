848f19e4437911094f5a21f6c3f3793366c7072e
kawji <24.supavich@gmail.com>
2025-08-27 21:27:34 +0700

    Update layout.tsx

--- anime-kaso/src/app/layout.tsx
+++ anime-kaso/src/app/layout.tsx
@@ -4,6 +4,23 @@ import './globals.css';
 import Navbar from '@/components/feature/layout-main/navbar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+import { Noto_Sans_Thai, Noto_Sans } from 'next/font/google'
+
+const notoSansThai = Noto_Sans_Thai({
+  subsets: ['thai'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-thai',
+})
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-latin',
+})
+
+
 
 export default function RootLayout({
   children,
@@ -13,7 +30,7 @@ export default function RootLayout({
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <html lang="en">
+    <html lang="th" className={`${notoSansThai.variable} ${notoSans.variable}`}>
       <body>
         <QueryClientProvider client={queryClient}>
           <Navbar />
