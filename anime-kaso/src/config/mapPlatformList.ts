b395e32451048198ffecc09813fed11a2518d316
kawji <24.supavich@gmail.com>
2025-08-27 21:27:50 +0700

    Update mapPlatformList.ts

--- anime-kaso/src/config/mapPlatformList.ts
+++ anime-kaso/src/config/mapPlatformList.ts
@@ -5,24 +5,24 @@ import Bilibili from "@/components/ui/icons/bilibili";
 import Crunchyroll from "@/components/ui/icons/crunchyroll";
 import Funimation from "@/components/ui/icons/funimation";
 import Iq from "@/components/ui/icons/iq";
-import { JSX } from "react";
 
 
 export type ObjPlatform = {
     pathname: React.ComponentType;
     name: string;
+    url:string;
 }
 
 export type recordPlatform = Record<string,ObjPlatform>
 
 export const mapPlatform:recordPlatform = {
-    netflix: { pathname: Netflix ,name:'Netflix' },
-    youtube: { pathname:YouTube ,name:'YouTube' },
-    amazon: { pathname:AmazonPrimeVideo ,name:'Amazon Prime Video' },
-    bilibili: { pathname:Bilibili ,name:'Bilibili' },
-    crunchyroll: { pathname:Crunchyroll ,name:'Crunchyroll' },
-    funimation: { pathname:Funimation ,name:'Funimation' },
-    iqiy: { pathname:Iq ,name:'iQIYI' },
+    netflix: { pathname: Netflix ,name:'Netflix' ,url:'https://www.netflix.com/th/' },
+    youtube: { pathname:YouTube ,name:'YouTube' ,url:'https://www.youtube.com/' },
+    amazon: { pathname:AmazonPrimeVideo ,name:'Amazon Prime Video' ,url:'https://www.primevideo.com/' },
+    bilibili: { pathname:Bilibili ,name:'Bilibili' ,url:'https://www.bilibili.tv/th/' },
+    crunchyroll: { pathname:Crunchyroll ,name:'Crunchyroll' ,url:'https://www.crunchyroll.com/' },
+    funimation: { pathname:Funimation ,name:'Funimation' ,url:'https://www.crunchyroll.com/' },
+    iqiy: { pathname:Iq ,name:'iQIYI' ,url:'https://www.iq.com/' },
 
 } as const;
 
