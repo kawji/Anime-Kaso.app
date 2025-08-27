07a6d081bdbd12e5060b1ea7edcc6589a048436f
kawji <24.supavich@gmail.com>
2025-08-27 21:27:52 +0700

    Update sidebarMenu.ts

--- anime-kaso/src/config/sidebarMenu.ts
+++ anime-kaso/src/config/sidebarMenu.ts
@@ -1,11 +1,12 @@
+
 export const sidebarMenu = [
-    {section:'หน้าแรก', path:'/'},
-    {section:'อนิเมะทั้งหมด', path:'/anime'},
-    {section:'กำลังฉาย', path:'/show'},
-    {section:'ยังไม่ฉาย', path:'/noshow'},
-    {section:'อนิเมะไม่ 18+', path:'/animeNoHentei'},
-    {section:'อนิเมะ - Hentai', path:'/animehentai'},
-    {section:'อนิเมะ - Movie', path:'/animemovie'},
-    {section:'ยังไม่จบ', path:'/noend'},
-    {section:'จบแล้ว', path:'/end'},
+    {section:'หน้าแรก', path:'/' ,title:'อนิเมะล่าสุด'},
+    {section:'อนิเมะทั้งหมด', path:'/anime' ,title:'อนิเมะทั้งหมด'},
+    {section:'กำลังฉาย', path:'/show' ,title:'อนิเมะกำลังฉาย'},
+    {section:'ยังไม่ฉาย', path:'/noshow' ,title:'อนิเมะยังไม่ฉาย'},
+    {section:'อนิเมะไม่ 18+', path:'/animeNoHentei' ,title:'อนิเมะไม่ 18+' },
+    {section:'อนิเมะ - Hentai', path:'/animehentai' ,title:'อนิเมะ Hentai' },
+    {section:'อนิเมะ - Movie', path:'/animemovie' ,title:'อนิเมะ Movie' },
+    {section:'ยังไม่จบ', path:'/noend' ,title:'อนิเมะยังไม่จบ' },
+    {section:'จบแล้ว', path:'/end' ,title:'อนิเมะจบเเล้ว' },
 ]
