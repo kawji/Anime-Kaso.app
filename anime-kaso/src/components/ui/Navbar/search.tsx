c7f8e073f72c841acd8128e95ae5fcaa8ea9a0e9
kawji <24.supavich@gmail.com>
2025-08-28 21:56:49 +0700

    Update search.tsx

--- anime-kaso/src/components/ui/Navbar/search.tsx
+++ anime-kaso/src/components/ui/Navbar/search.tsx
@@ -3,9 +3,9 @@
 
 export default function SearchAnime() {
     return(
-        <div className=" flex items-center gap-3 justify-end grow px-20">
+        <div className=" flex items-center gap-3 justify-end grow lg:px-20 sm:px-10 pl-5 ">
 
-            <input type='text' placeholder="ค้นหาอนิเมะ" className="pl-3 outline-none w-[60px] sm:w-[80px] lg:w-[240px] h-[30px] sm:h-[35px] rounded-md border border-white/30 bg-[#161515]  " />
+            <input type='text' placeholder="ค้นหาอนิเมะ" className="pl-3 text-sm sm:text-[16px] shrink-0 outline-none min-w-[60px] max-w-[180px] w-full sm:w-[200px] lg:w-[240px] h-[30px] sm:h-[35px] rounded-md border border-white/30 bg-[#161515]  " />
         </div>
     );
 }
