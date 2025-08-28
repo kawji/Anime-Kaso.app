3e495fcf9f56bee6b736bd7614a25c15a259af19
kawji <24.supavich@gmail.com>
2025-08-28 21:56:46 +0700

    Update logo.tsx

--- anime-kaso/src/components/ui/Navbar/logo.tsx
+++ anime-kaso/src/components/ui/Navbar/logo.tsx
@@ -8,10 +8,10 @@ type PropsLogo = {
 export default function Logo({text}:PropsLogo) {
     return(
         <Link href='/' className="flex items-center p-0 shrink-0">
-            <span className={clsx(" font-bold ",text? text:'text-3xl')}>
+            <span className={clsx(" font-bold ",text? text:'lg:text-3xl sm:text-2xl text-[16px]')}>
                 Anime&nbsp;-&nbsp;
             </span>
-            <span className={clsx("text-[var(--primary-text)] font-bold ",text? text:'text-3xl')}  >
+            <span className={clsx("text-[var(--primary-text)] font-bold ",text? text:'lg:text-3xl sm:text-2xl text-[16px] ')}  >
                 Kaso.com
             </span>
         </Link>
